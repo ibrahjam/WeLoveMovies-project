61220ac03319c7817d880033c520f55b29b602d8
ibrahjam <ibrahjam@gmail.com>
2022-04-30 19:43:07 -0500

    third

--- src/errors/methodNotAllowed.js
+++ src/errors/methodNotAllowed.js
@@ -1,7 +1,7 @@
-function methodNotAllowed(request, response, next) {
+function methodNotAllowed(req, res, next) {
     next({
       status: 405,
-      message: `${request.method} not allowed for ${request.originalUrl}`,
+      message: `${req.method} not allowed for ${req.originalUrl}`,
     });
   }
   
